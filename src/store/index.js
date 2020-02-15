b24f45f489b234294b4f08cc2206d911164d9bfa
Willian Souza <willianaru@gmail.com>
2020-02-15 16:35:13 -0300

    Aula 12

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,9 @@ import { createStore } from 'redux';
 
 import rootreducer from './modules/rootReducer';
 
-const store = createStore(rootreducer);
+const enhancer =
+  process.env.NODE_ENV === 'development' ? console.tron.createEnhancer() : null;
+
+const store = createStore(rootreducer, enhancer);
 
 export default store;
