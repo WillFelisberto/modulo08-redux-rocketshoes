50f635bd25ae34985f627a3642b0a361407e30aa
Willian Souza <willianaru@gmail.com>
2020-02-14 21:05:13 -0300

    first commit

---
+++ src/pages/Cart/index.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+
+import { Container, ProductTable, Total } from './styles';
+
+export default function Cart() {
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTDE</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-masculino-polo-joy-casual-em-lona/42/FSO-0012-342/FSO-0012-342_detalhe2.jpg?ims=326x"
+                alt="Tênis"
+              />
+            </td>
+            <td>
+              <strong>Tenis topper</strong>
+              <span>R$123,412</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$232,33</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$23123,2</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
+}
